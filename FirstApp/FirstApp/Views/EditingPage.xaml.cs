d591de5993b0b6204c5b80adf0560d3f88f18180
B00zer0 <Artur.krasickov@yandex.ru>
2023-01-28 17:22:09 +0300

    создал страницу редактирования контейнеров

---
+++ FirstApp/FirstApp/Views/EditingPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using FirstApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace FirstApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditingPage : ContentPage
+    {
+        
+        public EditingPage()
+        {
+            InitializeComponent();
+            
+        }
+        protected override async void OnAppearing()
+        {
+            try
+            {
+
+                collectionView.ItemsSource = await ContainersDB.GetContainersAsync();
+            }
+            catch(Exception ex) { _ = ex; }
+            base.OnAppearing();
+        }
+
+        private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
